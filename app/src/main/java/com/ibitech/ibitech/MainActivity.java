8ce5e251d476c08e87003ae5b9d9f48052dd865b
s216100801 <jubhelaasavela@gmail.com>
2018-04-03 04:54:20 +0200

    UPDATE

--- app/src/main/java/com/ibitech/ibitech/MainActivity.java
+++ app/src/main/java/com/ibitech/ibitech/MainActivity.java
@@ -2,6 +2,8 @@ package com.ibitech.ibitech;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
